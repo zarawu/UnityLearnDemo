cededb8f4158ea66f2ae33f937dd07395e4aac50
zarawu <zara_wu@163.com>
2015-03-09 21:44:26 +0800

    初始化

---
+++ Project/Assets/Scripts/MainManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainManager : MonoBehaviour {
+
+	private string descInfo;
+	// Use this for initialization
+	void Start () {
+		descInfo = "工程说明：每个Scene对应一个练习项，在OnGUI()中给出相应的button链接到对应Scene";
+	}
+	
+	void OnGUI(){
+		GUI.TextArea (new Rect (350, 20, 200, 300),descInfo);
+		if (GUI.Button (new Rect (20, 20, 120, 40), "AnimatorScene")) {
+			Application.LoadLevel("AnimatorScene");
+		}
+	}
+}
