dabc4510ea91bbadadbb2024e6744e05aba945e3
Molo Xiao <molo.xiao@gmail.com>
2015-03-10 16:15:27 +0800

    增加炉火光的闪动

---
+++ Project/Assets/Graphics/Lighting/Script/LightControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControl : MonoBehaviour {
+
+	private Light light;
+	private float add = 0.1f;
+	
+	void Start () {
+		light = GetComponent<Light>();
+	}
+
+	void Update () {
+		light.intensity += add;
+		if(light.intensity > 6.0f) {
+			add = -add;
+		}else if(light.intensity < 5.0f) {
+			add = -add;
+		}
+	}
+}
